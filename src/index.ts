f61503a636ed22734dc283526a2334ba353b98fb
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-11-28 22:40:39 +0300

    feat: add headers

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,18 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
 
   next();
 })
